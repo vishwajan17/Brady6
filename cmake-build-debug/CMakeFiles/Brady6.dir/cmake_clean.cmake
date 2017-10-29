d2d00ae63864e9ae238adad4875bfd1d4d08d0cc
Vishwanatha Janaswamy <vishwajan@gmail.com>
2017-10-29 12:32:00 -0400

    Initial setup

---
+++ cmake-build-debug/CMakeFiles/Brady6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Brady6.dir/main.cpp.o"
+  "Brady6.pdb"
+  "Brady6"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Brady6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
